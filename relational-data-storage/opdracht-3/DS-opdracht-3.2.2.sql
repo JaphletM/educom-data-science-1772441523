818a926b48503a01853eb46dde7858f99a52ed3f
Japhlet Mputeni <grace.mputeni@hotmail.co.uk>
2026-03-10 15:22:25 +0100

    #4 3.2.2

--- relational-data-storage/opdracht-3/DS-opdracht-3.2.2.sql
+++ relational-data-storage/opdracht-3/DS-opdracht-3.2.2.sql
@@ -1,1 +1,4 @@
-SELECT DISTINCT name FROM mhl_cities
+SELECT s.name, s.straat, s.huisnr , s.postcode 
+FROM mhl_suppliers s
+JOIN mhl_membertypes m ON s.membertype=m.id
+WHERE m.name IN ('Gold','Silver', 'Bronze','GEEN INTERRESSE');
