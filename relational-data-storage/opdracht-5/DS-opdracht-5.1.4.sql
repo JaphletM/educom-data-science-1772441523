8a42b072ec7b74e86e1a46695b4aeb6d24fe4819
Japhlet Mputeni <grace.mputeni@hotmail.co.uk>
2026-03-16 10:46:43 +0100

    #6 5.1.3

--- relational-data-storage/opdracht-5/DS-opdracht-5.1.4.sql
+++ relational-data-storage/opdracht-5/DS-opdracht-5.1.4.sql
@@ -1,11 +1,10 @@
 SELECT
-    mhl_cities.name AS Stad,
-    COUNT(IF(mhl_membertypes.name = 'Gold', 1, NULL)) Gold,
-    COUNT(IF(mhl_membertypes.name = 'Silver', 1, NULL)) Silver,
-    COUNT(IF(mhl_membertypes.name = 'Bronze', 1, NULL)) 'Bronze',
-    COUNT(IF (mhl_membertypes.name NOT IN ('Gold','Silver','Bronze'), 1, NULL)) 'Other'
-FROM mhl_suppliers
-JOIN mhl_membertypes ON mhl_suppliers.membertype=mhl_membertypes.id
-JOIN mhl_cities ON city_ID = mhl_cities.ID
-GROUP BY city_id
-ORDER BY Gold DESC, Silver DESC, Bronze DESC, Other DESC;
+    ms.name AS name,
+    SUM(mh.hitcount) AS numhits,
+    COUNT(mh.month) AS nummonths,
+    ROUND(AVG(mh.hitcount),0) AS avgpermonth
+FROM mhl_hitcount mh
+JOIN mhl_suppliers ms ON ms.id=mh.supplier_ID
+GROUP BY name
+HAVING numhits > 100
+ORDER BY avgpermonth DESC
