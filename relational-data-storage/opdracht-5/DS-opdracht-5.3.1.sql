d9e2c919a93849cbc591df47017ae186fe891771
Japhlet Mputeni <grace.mputeni@hotmail.co.uk>
2026-03-16 12:17:54 +0100

    #6 5.3.1

---
+++ relational-data-storage/opdracht-5/DS-opdracht-5.3.1.sql
@@ -0,0 +1,10 @@
+CREATE VIEW directie 
+AS 
+SELECT  c.supplier_ID, 
+        c.name AS contact, 
+        c.contacttype AS functie,
+        d.name AS department 
+FROM mhl_contacts c 
+JOIN mhl_departments d ON c.department=d.id 
+WHERE d.name='Directie' 
+OR c.contacttype LIKE '%directeur%';
