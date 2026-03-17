3d7a66c34be576b7280770f8f845e4fe5edfd810
Japhlet Mputeni <grace.mputeni@hotmail.co.uk>
2026-03-17 10:25:48 +0100

    #7 6.2.2

--- relational-data-storage/opdracht-5/DS-opdracht-6.1.1.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    h.year,
-    h.month,
-    COUNT(DISTINCT h.supplier_ID) AS aantal_leveranciers,
-    SUM(h.hitcount) AS totale_hitcount
-FROM mhl_hitcount h
-GROUP BY h.year, h.month
-ORDER BY h.year, h.month;
